31190b9cca88c60d4fe0994d15bd48ac684094ee
svenngjeto <svenn.gjeto@unit.no>
2020-01-30 10:28:21 +0100

    Added more functions functions

---
+++ src/main/java/no/unit/nva/amazon/s3/ListPartsHandler.java
@@ -0,0 +1,17 @@
+package no.unit.nva.amazon.s3;
+
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+
+import java.util.Map;
+
+
+public class ListPartsHandler implements RequestHandler<Map<String, Object>, GatewayResponse> {
+
+    @Override
+    public GatewayResponse handleRequest(Map<String, Object> input, Context context) {
+        return null;
+    }
+
+}
